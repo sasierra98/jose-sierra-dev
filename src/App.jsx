3b1ac88d42d8cf2d5cc77b41cd67892ebe156888
Jose Andres Sierra Arrieta <joseandresmayo@gmail.com>
2023-09-18 00:26:00 -0500

    Refactor: App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+import { NavBar } from './components/Header/Header';
+import { Box, ThemeProvider} from '@mui/material';
+import { theme } from './styles';
+import { MainHero } from './components/MainHero/MainHero';
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+    <Box>
+      <header>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat" />
+      </header>
+      <NavBar />
+      <MainHero />
+      <Box component={"section"}>
+
+      </Box>
+    </Box>
+    </ThemeProvider>
+  )
+}
+
+export default App;
