c7f88c233557d9fe6645a13adb2bb606b7c307f4
Jose Andres Sierra Arrieta <joseandresmayo@gmail.com>
2023-09-18 00:24:20 -0500

    Add: Media queries added in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '0px',
+        'sm': '600px',
+        'md': '900px',
+        'lg': '1020px',
+        'xl': '1200px',
+        '2xl': '1536px',
+      }
+    },
   },
   plugins: [],
 }
