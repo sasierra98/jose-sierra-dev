ca2d2522365907de5b6a9353ca4f9dba188a9f29
Jose Andres Sierra Arrieta <joseandresmayo@gmail.com>
2023-09-18 00:27:03 -0500

    Add: Global Styles

---
+++ src/styles.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: 'Montserrat'
+  },
+  palette: {
+    primary: {
+      main: '#EEF2E5',
+      dark: '#CAD5CA',
+      darker: '#8D9E8D'
+    },
+    secondary: {
+      main: '#0252CD'
+    }
+  }
+})
