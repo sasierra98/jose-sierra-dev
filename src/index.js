7ace3165df2377b97cf67050087f2f58a49bf5f9
Jose Andres Sierra Arrieta <joseandresmayo@gmail.com>
2023-10-02 19:25:09 -0500

    Update: Name and route of Home

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { MainPage } from "./pages";
+import { MainPage } from "./pages/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,14 @@
 import { Box, ThemeProvider } from "@mui/material";
 
-import { theme } from "../styles";
-import { NavBar } from "../components/NavBar/NavBar";
-import { MainHero } from "../components/MainPage/MainHero/MainHero";
-import { Services } from "../components/MainPage/Services/Services";
+import { theme } from "../../styles";
+import { NavBar } from "../../components/NavBar/NavBar";
+import { MainHero } from "../../components/MainPage/MainHero";
+import { Services } from "../../components/MainPage/Services";
+import { Carrousel } from "../../components/Carrousel";
+import { homeInfo } from "./homeInfo";
 
 export const MainPage = () => {
+  const {carrouselInfo} = homeInfo();
   return (
     <ThemeProvider theme={theme}>
       <Box>
@@ -18,6 +21,7 @@ export const MainPage = () => {
         <NavBar />
         <MainHero />
         <Services />
+        <Carrousel data={carrouselInfo} />
       </Box>
     </ThemeProvider>
   );
