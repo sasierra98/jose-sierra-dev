00e29e664c8d9a82cdd2595c50bc133b12e6f0a0
Jose Andres Sierra Arrieta <joseandresmayo@gmail.com>
2023-10-07 00:07:01 -0500

    Add: Components styles

--- src/styles.js
+++ src/styles.js
@@ -11,6 +11,9 @@ export const palette = {
   },
   common: {
     white: "#ffffff"
+  },
+  custom: {
+    subtitle: "#151E2C"
   }
 }
 
@@ -19,4 +22,66 @@ export const theme = createTheme({
     fontFamily: "Montserrat",
   },
   palette: palette,
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        root: ({ ownerState, theme }) => ({
+          ...ownerState.variant === "h1" && {
+            [theme.breakpoints.up("xs")]: {
+              fontSize: "60px"
+            },
+            [theme.breakpoints.up("xl")]: {
+              fontSize: "70px"
+            }
+          },
+          ...ownerState.variant === "h2" && {
+            [theme.breakpoints.up("xs")]: {
+              fontSize: "32px"
+            },
+            [theme.breakpoints.up("md")]: {
+              fontSize: "40px"
+            },
+            [theme.breakpoints.up("xl")]: {
+              fontSize: "50px"
+            }
+          },
+          ...ownerState.variant === "h3" && {
+            [theme.breakpoints.up("xs")]: {
+              fontSize: "24px"
+            },
+            [theme.breakpoints.up("md")]: {
+              fontSize: "32px"
+            },
+            [theme.breakpoints.up("xl")]: {
+              fontSize: "40px"
+            }
+          },
+          ...ownerState.variant === "h4" && {
+            [theme.breakpoints.up("xs")]: {
+              fontSize: "24px",
+              fontWeight: 700,
+              color: theme.palette.custom.subtitle
+            },
+            [theme.breakpoints.up("xl")]: {
+              fontSize: "32px",
+              fontWeight: 700,
+              color: theme.palette.custom.subtitle
+            }
+          },
+          ...ownerState.variant === "h5" && {
+            [theme.breakpoints.up("xs")]: {
+              fontSize: "12px",
+              fontWeight: 600,
+              color: "#9D9D9D"
+            },
+            [theme.breakpoints.up("xl")]: {
+              fontSize: "16px",
+              fontWeight: 600,
+              color: "#9D9D9D"
+            }
+          }
+        })
+      }
+    }
+  }
 });
